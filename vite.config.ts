a7bb7135128b50d65cd34e932900bd80ba2fbbaf
Devi Sri Prasad Puvvala <devisriprasad9652@gmail.com>
2025-07-26 16:49:05 +0530

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/Portfolio/",
   server: {
     host: "::",
     port: 8080,
